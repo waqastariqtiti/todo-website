b37045add8a0cb197e9771e8b44d790ebcf5993c
waqas <tariqwaqasttii@gmail.com>
2024-08-15 23:45:53 +0500

    firebase added to login page

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import {
     getAuth,
     createUserWithEmailAndPassword,
-  } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+} from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 
-  const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyD0eeQSG-_qpSg4i0V8vMsbMp-mFa9fz6A",
     authDomain: "todo-web-c2500.firebaseapp.com",
     projectId: "todo-web-c2500",
@@ -13,11 +12,11 @@ import {
     messagingSenderId: "341838122470",
     appId: "1:341838122470:web:a771493442dc6839631a27",
     measurementId: "G-WDH5GX9P5K"
-  };
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 const button = document.getElementById("submit");
 button.addEventListener("click", handleLogin);
@@ -25,18 +24,19 @@ button.addEventListener("click", handleLogin);
 function handleLogin(event) {
   event.preventDefault();
   console.log("Button clicked, handling login");
+  const emailValue = document.getElementById("email").value;
+  const passValue = document.getElementById("password").value;
 
-  // Get email and password values here
-  var emailvalue = document.getElementById("email").value;
-  var pasvalue = document.getElementById("password").value;
-
-  createUserWithEmailAndPassword(auth, emailvalue, pasvalue)
+  createUserWithEmailAndPassword(auth, emailValue, passValue)
     .then((userCredential) => {
-      // Signed in
+
       console.log("User created:", userCredential.user);
+
+      document.getElementById("email").value = "";
+      document.getElementById("password").value = "";
     })
     .catch((error) => {
       console.error("Error signing up:", error);
+
     });
-    var emailvalue = emailvalue.value = "";
 }
