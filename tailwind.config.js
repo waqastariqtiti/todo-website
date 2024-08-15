2ceb2c591729551c18b6ef3bee5f6b3f86ec1577
waqas <tariqwaqasttii@gmail.com>
2024-08-15 22:51:15 +0500

    update project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [],
-    theme: {
-      extend: {
-        screens: {
-          'sm-max': { 'max': '500px' },
-        },
-        height:{
-          '120' :'120px',
-        },
+  content: [],
+  theme: {
+    extend: {
+      screens: {
+        'sm-max': { 'max': '500px' },
+      },
+      height:{
+        '120' :'120px',
       },
     },
-    plugins: [],
-  }
-  
-  
+  },
+  plugins: [],
+}
+
+
