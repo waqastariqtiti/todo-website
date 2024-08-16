36e3877c6060d58faf7af2ee1cca72b82e353c05
waqas <tariqwaqasttii@gmail.com>
2024-08-16 16:06:21 +0500

    last

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-import {
-    getAuth,
-    createUserWithEmailAndPassword,
-} from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyD0eeQSG-_qpSg4i0V8vMsbMp-mFa9fz6A",
-    authDomain: "todo-web-c2500.firebaseapp.com",
-    projectId: "todo-web-c2500",
-    storageBucket: "todo-web-c2500.appspot.com",
-    messagingSenderId: "341838122470",
-    appId: "1:341838122470:web:a771493442dc6839631a27",
-    measurementId: "G-WDH5GX9P5K"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyBGBzza0NotXsim6wosXbTJFLWQD3ZOviI",
+    authDomain: "todo-web2.firebaseapp.com",
+    projectId: "todo-web2",
+    storageBucket: "todo-web2.appspot.com",
+    messagingSenderId: "702972166109",
+    appId: "1:702972166109:web:46656c662b9ce5fcf08034",
+    measurementId: "G-T94B1MY9M0"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const auth = getAuth(app);       // Authentication
 
 const button = document.getElementById("submit");
 button.addEventListener("click", handleLogin);
