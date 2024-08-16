40a3f472fcfaf45e66839cfb4ed6073fa91de215
waqas <tariqwaqasttii@gmail.com>
2024-08-16 10:43:21 +0500

    home page done

--- home.js
+++ home.js
@@ -1,11 +1,32 @@
-var input = document.getElementById("text");
-
-
+let ID = 1;
 function add() {
-    document.getElementById("list").innerHTML += '${text.value} <button>delete</button>';
-    text.value = "";
+  var input = document.getElementById("text");
+  var list = document.getElementById("list");
+
+  if (input.value.trim() === "") {
+    alert("Please enter some text.");
+    return;
+  }
+
+  list.innerHTML += `<div id="addItem-${ID}" class="flex justify-between mx-5 my-5">${ID}: ${input.value} 
+      <button onclick="deleteAdded(${ID})" class="text-black bg-red-500 p-2 rounded-md">Delete</button>
+    </div>`;
+  text.value = "";
+  ID++;
 }
-function deleteAdded() {
-    document.getElementById("list").innerHTML = '<p class="ml-[36%]">add your work</p>';
-    
+function deleteAdded(itemID) {
+  const item = document.getElementById(`addItem-${itemID}`);
+  const restoreButton = document.getElementById(`restore`);
+  if (item) {
+    item.style.display = "none";
+    restoreButton.style.display = "inline";
+  }
+}
+function restore(itemID) {
+  const item = document.getElementById(`addItem-${itemID}`);
+  const restoreButton = document.getElementById(`restore`);
+  if (item) {
+    item.style.display = "inline";
+    restoreButton.style.display = "none";
+  }
 }
